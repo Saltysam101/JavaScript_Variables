5cb73edec72953fae1fff93c9ab27b58649b552f
Sam Zudell <s.zudell98@gmail.com>
2023-02-22 11:56:22 -0500

    made a lot of variables

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 console.log("Hello World!\n==========\n");
 console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+    "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
 // Exercise 1
+const firstName = "Sam"
+let lastName = "Zudell"
+var age = 24
+    // Exercise 2
+let language = "JavaScript"
+let createdYear = 1995
+let isCaseSensitive = true
+    // Exercise 3
 
-// Exercise 2
+let price = 19.99
+let isOnSale = false
+let salePercentage = 15
+let stock = 0
+let inStock = false
+let selectedSize = "M"
 
-// Exercise 3
+
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722
+let bookmark = 456
+let hasRead = true
